ccd9d0b985e9210d394b4712b3adc62fa0f5d0ee
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-11-01 22:16:11 +0100

    Handle operator precedence

--- xc/CodeAnalysis/Parser.cs
+++ xc/CodeAnalysis/Parser.cs
@@ -70,39 +70,38 @@ namespace Xlang.CodeAnalysis
             return new SyntaxTree(_diagnostics, expression, endOfFileToken);
         }
 
-        private ExpressionSyntax ParseTerm()
-        {
-            var left = ParseFactor();
-
-            while (Current.Kind == SyntaxKind.PlusToken ||
-                   Current.Kind == SyntaxKind.MinusToken)
-            {
-                var operatorToken = NextToken();
-                var right = ParseFactor();
-                left = new BinaryExpressionSyntax(left, operatorToken, right);
-            }
-        
-            return left;
-        }
-
-        private ExpressionSyntax ParseFactor()
+        public ExpressionSyntax ParseExpression(int parentPrecedence = 0)
         {
             var left = ParsePrimaryExpression();
-
-            while (Current.Kind == SyntaxKind.StarToken ||
-                   Current.Kind == SyntaxKind.SlashToken)
+            
+            while (true)
             {
+                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                if (precedence == 0 || precedence <= parentPrecedence)
+                    break;
+
                 var operatorToken = NextToken();
-                var right = ParsePrimaryExpression();
+                var right = ParseExpression(precedence);
                 left = new BinaryExpressionSyntax(left, operatorToken, right);
             }
-        
+
             return left;
         }
 
-        public ExpressionSyntax ParseExpression()
+        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
         {
-            return ParseTerm();
+            switch (kind)
+            {
+                case SyntaxKind.PlusToken:
+                case SyntaxKind.MinusToken:
+                    return 1;
+                case SyntaxKind.StarToken:
+                case SyntaxKind.SlashToken:
+                    return 2;
+
+                default:
+                    return 0;
+            }
         }
 
         public ExpressionSyntax ParsePrimaryExpression()
